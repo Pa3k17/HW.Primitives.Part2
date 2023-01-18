58fe41976754ec1bdb77e014ba2af1483fdae03b
Pavel <pa3k1979@gmail.com>
2023-01-18 23:30:39 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int initialAccount = 100;
+        int addend = 1100;
+
+         int bonus = 0;
+         if (addend > 1000) {
+             bonus = addend / 100;
+         }
+         int finalAccount = initialAccount + addend + bonus;
+
+        System.out.println("Итоговый счет: " + finalAccount);
+        System.out.println("Бонус: " + bonus);
+            }
+}
